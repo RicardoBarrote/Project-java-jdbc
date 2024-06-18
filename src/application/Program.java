2ececa286b221a6791108d42311c24fadda7c05f
RicardoBarrote <ricardoambscfc@gmail.com>
2024-06-18 10:46:00 -0300

    Class created (Seller) / testing the classes in the main program
    Signed-off-by: RicardoBarrote <ricardoambscfc@gmail.com>

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,19 @@
 package application;
 
+import java.util.Date;
+
+import model.entities.Department;
+import model.entities.Seller;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		
+		//Efetuando testes das classes criadas. 
+		Department dep = new Department(1, "Comercial");
+		Seller seller = new Seller(1, "Ricardo", "Ricardo@gmail.com", new Date(), 3000.0, dep);
 
+		System.out.println("Executado!, dados -> " + seller);
 	}
 
 }
